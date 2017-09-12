485533a13635be975bf7739adba067e622724e2f
Ryan Cunningham <31255454+ryan-cunningham@users.noreply.github.com>
2017-09-12 14:10:51 +0100

    Create ex27.py

---
+++ LPTHW/ex27.py
@@ -0,0 +1,38 @@
+# LPTHW: Exercise 27 - Memorizing Logic
+
+# This exercise is about memorising Truth terms.
+# Note: Any 'and' expression that has a 'False' is immediately False,
+#       Any 'or' expression that has a 'True' is immediately True
+
+not False               =   True
+not True                =   False
+
+True or False           =   True
+True or True            =   True
+False or True           =   True
+False or False          =   False
+
+True and False          =   False
+True and True           =   True
+False and True          =   False
+False and False         =   False
+
+not (True or False)     =   False
+not (True or True)      =   False
+not (False or True)     =   False
+not (False or False)    =   True
+
+not (True and False)    =   True
+not (True and True)     =   False
+not (False and True)    =   True
+not (False and False)   =   True
+
+1 != 0                  =   True
+1 != 1                  =   False
+0 != 1                  =   True
+0 != 0                  =   False
+
+1 == 0                  =   False
+1 == 1                  =   True
+0 == 1                  =   False
+0 == 0                  =   True
