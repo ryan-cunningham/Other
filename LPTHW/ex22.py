a2658868cc62289a1fa4bf818512e33856d3c391
Ryan Cunningham <31255454+ryan-cunningham@users.noreply.github.com>
2017-09-12 14:04:32 +0100

    Update ex22.py

--- LPTHW/ex22.py
+++ LPTHW/ex22.py
@@ -1,2 +1,24 @@
-# LPTHW: Exercise 22 - What Do You Know So Far?
+# LPTHW: Exercise 22 - What Do You Know So Far
 
+# what is an octothorpe?
+
+print("strings")
+
+variable1 = "Format"
+variable2 = "created variables"
+variable3 = "in variety of ways"
+
+print(variable1, f"strings with {variable2} " + variable3 + "." * 3)
+
+Also = """\nNewlines
+\tTabbed in...
+
+user input
+import argv
+reading files
+truncating files
+writing to files
+creating files
+copying files
+Functions
+"""
