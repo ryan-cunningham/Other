443fe5441984fbd6deb539a57745650145941de5
Ryan Cunningham <31255454+ryan-cunningham@users.noreply.github.com>
2017-09-12 19:51:11 +0100

    Update ex33.py

--- LPTHW/ex33.py
+++ LPTHW/ex33.py
@@ -16,3 +16,28 @@ print("The Numbers: ")
 
 for num in numbers:
     print(num)
+
+    
+    
+########################
+##### study drills #####
+########################
+
+def iterate(hi, step):
+    for i in range (0, hi, step):
+        print(f"At the top i is {i}")
+        numbers.append(i)
+        print("Numbers now:", numbers)
+        print(f"At the bottom i is {i}")
+
+hi = int(input("What is our range? (start point is 0): "))
+step = int(input("And the step-size?: "))
+
+numbers = []
+
+iterate(hi, step)
+
+print("The numbers:")
+
+for num in numbers:
+    print(num)
