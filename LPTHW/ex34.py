6a00ebd4787fec5705648645d71de6862aa20d56
Ryan Cunningham <31255454+ryan-cunningham@users.noreply.github.com>
2017-09-29 21:19:51 +0100

    Create ex34.py

---
+++ LPTHW/ex34.py
@@ -0,0 +1,29 @@
+# LPTHW: Exercise 34 - Accessing Elements of Lists
+
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(f"This is the list of animals: ",animals)
+
+ani1 = animals[1]
+print(f"The animal at index 1 is a {ani1}")
+
+ani2 = animals[2]
+print(f"The third animal is a {ani2}")
+
+ani0 = animals[0]
+print(f"The first animal is a {ani0}")
+
+ani3 = animals[3]
+print(f"The animal at index 3 is a {ani3}")
+
+ani4 = animals[4]
+print(f"The fifth animal is a {ani4}")
+
+ani2 = animals[2]
+print(f"The animal at index 2 is a {ani2}")
+
+ani5 = animals[5]
+print(f"The sixth animal is a {ani5}")
+
+ani4 = animals[4]
+print(f"The animal at index 4 is a {ani4}")
